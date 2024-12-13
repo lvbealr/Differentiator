866b6606faac43b7568366ca10400e9863feb196
lvbealr <lvbealr@mail.ru>
2024-12-13 07:07:28 +0300

    add token and var table struct

--- include/differentiator.h
+++ include/differentiator.h
@@ -15,7 +15,9 @@ enum diffError {
   NO_VALUE          = 1 << 4,
   FILE_ERROR        = 1 << 5,
   DUMP_FILE_ERROR   = 1 << 6,
-  BUFFER_ERROR      = 1 << 7
+  BUFFER_ERROR      = 1 << 7,
+  BAD_ALLOCATION    = 1 << 8,
+  BAD_POINTER       = 1 << 9
 };
 
 enum nodeType {
@@ -26,6 +28,7 @@ enum nodeType {
 
 #define OPERATOR(NAME, ...) NAME,
 enum Operation {
+  EMPTY_OPERATOR,
   #include "diffOperations.def"
 };
 #undef OPERATOR
@@ -51,25 +54,63 @@ union nodeData {
 struct diffNode {
   nodeType  type       = NUMERICAL_NODE;
   nodeData  nodeValue  = {.value = NAN};
+  size_t    depth      = 0;
+};
+
+struct Variable {
+  char     *variableName  = {};
+  double    variableValue = NAN;
+  Variable *nextVariable  = NULL;
 };
 
 struct variableTable {
-  char  *variableName  = {};
-  double variableValue = NAN;
+  size_t    variableCount = 0;
+  Variable *variableArray = NULL; // TODO
+};
+
+struct Buffer {
+  char  *filePath = {};
+  char  *text     = {};
+  size_t textSize = {};
+};
+
+typedef nodeType tokenType;
+
+struct Token {
+  char      *tokenPointer = NULL;
+  tokenType  type         = {};
+  Token     *nextToken    = NULL;
+};
+
+struct tokensData {
+  size_t count      = 0;
+  Token *tokenArray = NULL; // TODO
 };
 
 struct Differentiator {
   binaryTree<diffNode> diffTree    =             {};
   variableTable       *variables   =             {};
   diffError            errorStatus = NO_DIFF_ERRORS;
+  Buffer              *buffer      =             {};
+  tokensData          *tokens      =             {};
 };
 
 // FUNCTION PROTOTYPES //
-diffError diffInitialize(Differentiator *diff);
-diffError diffDestruct  (Differentiator *diff);
-diffError diffVerify    (Differentiator *diff);
+diffError diffInitialize  (Differentiator *diff, int argc, char *argv[], const char *filename,
+                                                                         const char *funcName,
+                                                                         int         line);
+diffError diffDestruct    (Differentiator *diff);
+diffError bufferInitialize(Differentiator *diff);
 
-diffError evalTree      (Differentiator *diff, double *treeValue);
+diffError evalTree        (Differentiator *diff, double *value);
+
+const operationInfo *findOperationBySymbol(const char *symbol);
+const Variable      *findWordInTable      (Differentiator *diff, const Variable *word);
+
+// FUNCTION FOR NODES //
+node<diffNode> *diffNodeCreate(node<diffNode> nodeData);
+// FUNCTION FOR NODES //
+
 // FUNCTION PROROTYPES //
 
 #endif // DIFFERENTIATOR_H_
--- src/differentiator.cpp
+++ src/differentiator.cpp
@@ -1,2 +1,86 @@
 #include "differentiator.h"
+#include "diffIO.h"
 #include "binaryTree.h"
+#include "DSL.h"
+#include "lexer.h"
+
+static const size_t MAX_VARIABLE_COUNT = 3;
+
+diffError diffInitialize(Differentiator *diff, int argc, char *argv[], const char *fileName,
+                                                                       const char *funcName,
+                                                                       int         line) {
+    customWarning(diff != NULL, DIFF_NULL_PTR);
+
+    treeInitialize      (&diff->diffTree);
+    treeInfoInitialize  (&diff->diffTree, fileName, funcName, line);
+    bufferInitialize    (diff);
+    parseConsole        (argc, argv, &diff->diffTree);
+    binaryTreeSetInfo   (&diff->diffTree);
+    
+    diff->buffer->filePath = (char *)"IO/input.txt";
+    readFromFile(diff);
+
+    diff->tokens = (tokensData *)calloc(1, sizeof(tokensData));
+    customWarning(diff->tokens != NULL, BAD_ALLOCATION);
+    diff->tokens->tokenArray = (Token *)calloc(diff->buffer->textSize, sizeof(Token)); // TODO
+    customWarning(diff->tokens->tokenArray != NULL, BAD_ALLOCATION);
+
+    diff->variables = (variableTable *)calloc(1, sizeof(variableTable));
+    customWarning(diff->variables != NULL, BAD_ALLOCATION);
+    diff->variables->variableArray = (Variable *)calloc(MAX_VARIABLE_COUNT, sizeof(Variable)); // TODO
+    customWarning(diff->variables->variableArray != NULL, BAD_ALLOCATION);
+
+    return NO_DIFF_ERRORS;
+}
+
+static double evalInternal(Differentiator *diff, node<diffNode> *rootNode) {
+    customWarning(diff     != NULL, DIFF_NULL_PTR);
+    customWarning(rootNode != NULL, NODE_NULL_PTR);
+
+    switch (rootNode->data.type) {
+        case NUMERICAL_NODE: 
+            {
+                return rootNode->data.nodeValue.value;
+            }
+        
+        case OPERATION_NODE:
+            {
+                double evalValue = NAN;
+
+                #define OPERATOR(name, symbol, priority, callFunction, diffFunction)       \
+                    if (rootNode->data.nodeValue.op == name) {                             \
+                        callFunction                                                       \
+                    }
+
+                #include "diffOperations.def"
+
+                #undef OPERATOR
+
+                return evalValue;
+            }
+
+        case VARIABLE_NODE:
+            {
+                      Variable  patternVariable = {.variableName = &rootNode->data.nodeValue.varIndex};
+                const Variable *findVariable    = findWordInTable(diff, &patternVariable);
+                return findVariable->variableValue;
+            }
+
+        default:
+            {
+                return NAN;
+            }
+    }
+
+    return NAN;
+}
+
+diffError evalTree (Differentiator *diff, double *value) {
+    customWarning(diff  != NULL, DIFF_NULL_PTR);
+    customWarning(value != NULL, BAD_POINTER);
+
+    *value = evalInternal(diff, diff->diffTree.root);
+
+    return NO_DIFF_ERRORS;
+}
+
